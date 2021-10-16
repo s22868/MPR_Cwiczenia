224e751c47a738a76e8b3daadcd82a20f1df3ba4
Mateusz Budzyński <s22868@pjwstk.edu.pl>
2021-10-16 15:07:07 +0200

    firs commit, start project

---
+++ src/main/java/pjatk/komputer/KomputerApplication.java
@@ -0,0 +1,13 @@
+package pjatk.komputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomputerApplication.class, args);
+	}
+
+}
