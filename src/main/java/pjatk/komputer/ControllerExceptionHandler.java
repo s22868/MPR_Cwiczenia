7555c93e80453b2f83391bcacc9451b20cf686a6
Mateusz Budzyński <s22868@pjwstk.edu.pl>
2021-11-13 16:31:45 +0100

    comment unused code

--- src/main/java/pjatk/komputer/ControllerExceptionHandler.java
+++ src/main/java/pjatk/komputer/ControllerExceptionHandler.java
@@ -1,17 +1,17 @@
-package pjatk.komputer;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class ControllerExceptionHandler {
-    @ExceptionHandler(ResourceNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(...);
-
-        return message;
-    }
-}
+//package pjatk.komputer;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public class ControllerExceptionHandler {
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+//    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+//        ErrorMessage message = new ErrorMessage(...);
+//
+//        return message;
+//    }
+//}
